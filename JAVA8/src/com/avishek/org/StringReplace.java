55c4a52f848ee888a1db78ac62ce1742c4d16163
ibm <ibm@IBM137-PC0DBS6F.in.ibm.com>
2018-04-06 12:42:35 +0530

    StringReplace version 2

--- JAVA8/src/com/avishek/org/StringReplace.java
+++ JAVA8/src/com/avishek/org/StringReplace.java
@@ -7,10 +7,34 @@ public class StringReplace {
 		
 	}
 	
+	public String charEncode(int offset, String str){  
+	      String result = "";
+	      if(offset<0)
+	        offset=26+offset;
+	      for(int i=0;i<str.length();i++){
+	         result+=(char)('a'+(str.charAt(i)-'a'+offset)%26);
+	      }
+	      return result;
+	    }
+	
+	public String replace(String str, int index, char replace){     
+	    if(str==null){
+	        return str;
+	    }else if(index<0 || index>=str.length()){
+	        return str;
+	    }
+	    char[] chars = str.toCharArray();
+	    chars[index] = replace;
+	    return String.valueOf(chars);       
+	}
+	
 	public static void main(String[] args) {
 		StringBuilder myName = new StringBuilder("avishek");
 		myName.setCharAt(3, 'j');
 		System.out.println(myName);
+		StringReplace strReplace = new StringReplace();
+		strReplace.charEncode(1, "avishek");
+		strReplace.replace("avishek", -1, 'c');
 	}
 
 }
